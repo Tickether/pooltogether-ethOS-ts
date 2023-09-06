40a79d2e68fcbaedd57f1d9fc578ed187d95ee33
Tickether <tickether@gmail.com>
2023-09-06 14:27:36 +0000

    init Pool!

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Pool Together!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); 

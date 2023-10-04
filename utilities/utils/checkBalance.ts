6d96bd00f6fabb0a4e55861089781ffeba6942cc
Tickether <tickether@gmail.com>
2023-10-04 19:05:50 +0000

    update checkBalance return

--- utilities/utils/checkBalance.ts
+++ utilities/utils/checkBalance.ts
@@ -1,10 +1,8 @@
 import { getBalance } from "./getBalance"
 
-export const checkBalance = async (contract: string, decimals: number, amount: string) => {
+export const checkBalance = async (contract: string, symbol: string, decimals: number, amount: string) => {
     const balance = await getBalance(contract, decimals)
-    if (Number(balance) >= Number(amount)) {
-        return true
-    } else {
-        return false
+    if (Number(balance) < Number(amount)) {
+        return `Not enough ${symbol} in wallet`
     }
 }
